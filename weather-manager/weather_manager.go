428c5914b53966f08206f9b080c584e2bd1b4910
AJ Johnson <54604582+AjJ132@users.noreply.github.com>
2023-07-17 12:59:35 -0700

    added working future weather http void

--- weather-manager/weather_manager.go
+++ weather-manager/weather_manager.go
@@ -10,6 +10,7 @@ import (
 
 //correct format for calling this script is below
 //http://localhost:8080/realtime-weather?location=30144
+//http://localhost:8080/forecast?location=London
 
 func main() {
 
@@ -21,6 +22,10 @@ func main() {
 
 	http.HandleFunc("/realtime-weather", func(w http.ResponseWriter, r *http.Request) {
 		handler.getRealtimeWeather(w, r)
+	})
+
+	http.HandleFunc("/forecast", func(w http.ResponseWriter, r *http.Request) {
+		handler.getFutureWeather(w, r)
 	})
 
 	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
@@ -56,6 +61,32 @@ func (h *Handler) getRealtimeWeather(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(weather)
 
+}
+
+func (h *Handler) getFutureWeather(w http.ResponseWriter, r *http.Request) {
+	location := r.URL.Query().Get("location")
+	if location == "" {
+		http.Error(w, "location parameter is required", http.StatusBadRequest)
+		return
+	}
+	url := fmt.Sprintf("https://weatherapi-com.p.rapidapi.com/forecast.json?q=%s&days=1", location)
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "e6552f5551msh8af99331b1d77c7p16e6c2jsn105a4cbb1fbe")
+	req.Header.Add("X-RapidAPI-Host", "weatherapi-com.p.rapidapi.com")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+	body, _ := io.ReadAll(res.Body)
+
+	var forecast WeatherForecast
+	_ = json.Unmarshal(body, &forecast)
+
+	fmt.Println("Successfully got forecast")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(forecast)
 }
 
 type RealtimeWeather struct {
@@ -98,4 +129,135 @@ type RealtimeWeather struct {
 		GustMph    float64 `json:"gust_mph"`
 		GustKph    float64 `json:"gust_kph"`
 	} `json:"current"`
+}
+
+type WeatherForecast struct {
+	Location Location `json:"location"`
+	Current  Current  `json:"current"`
+	Forecast Forecast `json:"forecast"`
+}
+
+type Location struct {
+	Name           string  `json:"name"`
+	Region         string  `json:"region"`
+	Country        string  `json:"country"`
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	TzID           string  `json:"tz_id"`
+	LocaltimeEpoch int64   `json:"localtime_epoch"`
+	Localtime      string  `json:"localtime"`
+}
+
+type Current struct {
+	LastUpdatedEpoch int       `json:"last_updated_epoch"`
+	LastUpdated      string    `json:"last_updated"`
+	TempC            float64   `json:"temp_c"`
+	TempF            float64   `json:"temp_f"`
+	IsDay            int       `json:"is_day"`
+	Condition        Condition `json:"condition"`
+	WindMph          float64   `json:"wind_mph"`
+	WindKph          float64   `json:"wind_kph"`
+	WindDegree       int       `json:"wind_degree"`
+	WindDir          string    `json:"wind_dir"`
+	PressureMb       float64   `json:"pressure_mb"`
+	PressureIn       float64   `json:"pressure_in"`
+	PrecipMm         float64   `json:"precip_mm"`
+	PrecipIn         float64   `json:"precip_in"`
+	Humidity         int       `json:"humidity"`
+	Cloud            int       `json:"cloud"`
+	FeelslikeC       float64   `json:"feelslike_c"`
+	FeelslikeF       float64   `json:"feelslike_f"`
+	VisKm            float64   `json:"vis_km"`
+	VisMiles         float64   `json:"vis_miles"`
+	UV               int       `json:"uv"`
+	GustMph          float64   `json:"gust_mph"`
+	GustKph          float64   `json:"gust_kph"`
+}
+
+type Forecast struct {
+	Forecastday []ForecastDay `json:"forecastday"`
+}
+
+type ForecastDay struct {
+	Date      string `json:"date"`
+	DateEpoch int64  `json:"date_epoch"`
+	Day       Day    `json:"day"`
+	Astro     Astro  `json:"astro"`
+	Hour      []Hour `json:"hour"`
+}
+
+type Day struct {
+	MaxtempC          float64   `json:"maxtemp_c"`
+	MaxtempF          float64   `json:"maxtemp_f"`
+	MintempC          float64   `json:"mintemp_c"`
+	MintempF          float64   `json:"mintemp_f"`
+	AvgtempC          float64   `json:"avgtemp_c"`
+	AvgtempF          float64   `json:"avgtemp_f"`
+	MaxwindMph        float64   `json:"maxwind_mph"`
+	MaxwindKph        float64   `json:"maxwind_kph"`
+	TotalprecipMm     float64   `json:"totalprecip_mm"`
+	TotalprecipIn     float64   `json:"totalprecip_in"`
+	TotalsnowCm       float64   `json:"totalsnow_cm"`
+	AvgvisKm          float64   `json:"avgvis_km"`
+	AvgvisMiles       float64   `json:"avgvis_miles"`
+	Avghumidity       float64   `json:"avghumidity"`
+	DailyWillItRain   int       `json:"daily_will_it_rain"`
+	DailyChanceOfRain string    `json:"daily_chance_of_rain"`
+	DailyWillItSnow   int       `json:"daily_will_it_snow"`
+	DailyChanceOfSnow string    `json:"daily_chance_of_snow"`
+	Condition         Condition `json:"condition"`
+	UV                float64   `json:"uv"`
+}
+
+type Astro struct {
+	Sunrise          string `json:"sunrise"`
+	Sunset           string `json:"sunset"`
+	Moonrise         string `json:"moonrise"`
+	Moonset          string `json:"moonset"`
+	MoonPhase        string `json:"moon_phase"`
+	MoonIllumination string `json:"moon_illumination"`
+	IsMoonUp         int    `json:"is_moon_up"`
+	IsSunUp          int    `json:"is_sun_up"`
+}
+
+type Hour struct {
+	TimeEpoch    int       `json:"time_epoch"`
+	Time         string    `json:"time"`
+	TempC        float64   `json:"temp_c"`
+	TempF        float64   `json:"temp_f"`
+	IsDay        int       `json:"is_day"`
+	Condition    Condition `json:"condition"`
+	WindMph      float64   `json:"wind_mph"`
+	WindKph      float64   `json:"wind_kph"`
+	WindDegree   int       `json:"wind_degree"`
+	WindDir      string    `json:"wind_dir"`
+	PressureMb   float64   `json:"pressure_mb"`
+	PressureIn   float64   `json:"pressure_in"`
+	PrecipMm     float64   `json:"precip_mm"`
+	PrecipIn     float64   `json:"precip_in"`
+	Humidity     int       `json:"humidity"`
+	Cloud        int       `json:"cloud"`
+	FeelslikeC   float64   `json:"feelslike_c"`
+	FeelslikeF   float64   `json:"feelslike_f"`
+	WindchillC   float64   `json:"windchill_c"`
+	WindchillF   float64   `json:"windchill_f"`
+	HeatindexC   float64   `json:"heatindex_c"`
+	HeatindexF   float64   `json:"heatindex_f"`
+	DewpointC    float64   `json:"dewpoint_c"`
+	DewpointF    float64   `json:"dewpoint_f"`
+	WillItRain   int       `json:"will_it_rain"`
+	ChanceOfRain string    `json:"chance_of_rain"`
+	WillItSnow   int       `json:"will_it_snow"`
+	ChanceOfSnow string    `json:"chance_of_snow"`
+	VisKm        float64   `json:"vis_km"`
+	VisMiles     float64   `json:"vis_miles"`
+	GustMph      float64   `json:"gust_mph"`
+	GustKph      float64   `json:"gust_kph"`
+	UV           int       `json:"uv"`
+}
+
+type Condition struct {
+	Text string `json:"text"`
+	Icon string `json:"icon"`
+	Code int    `json:"code"`
 }

f5e385166407f42763cd87d433beb1a305c1c336
AJ Johnson <54604582+AjJ132@users.noreply.github.com>
2023-07-18 08:23:39 -0700

    Containerized mongo db and dbmanager

--- cache-database/dbmanager/db_manager.go
+++ cache-database/dbmanager/db_manager.go
@@ -2,16 +2,55 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func main() {
+type Item struct {
+	Data interface{} `bson:"data,omitempty"`
+}
 
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	mongoUri := os.Getenv("MONGODB_URI")
+	if mongoUri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable.")
+	}
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(mongoUri))
+
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(ctx)
+
+	collection := client.Database("test").Collection("documents")
+
+	http.HandleFunc("/save", func(w http.ResponseWriter, r *http.Request) {
+		var item Item
+		err := json.NewDecoder(r.Body).Decode(&item)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		ctx, cancel = context.WithTimeout(context.Background(), 5*time.Minute)
+		defer cancel()
+		_, err = collection.InsertOne(ctx, item)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "Item saved successfully in database.12345 ")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }

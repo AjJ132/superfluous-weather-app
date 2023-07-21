e3cb0c8ac8b61bf7ee66d18c9b2803da6e7b8dba
AJ Johnson <54604582+AjJ132@users.noreply.github.com>
2023-07-21 15:57:01 -0400

    Updated Dockerfile. Ready for K8S

--- front-end/superfluous-weather-app/main.js
+++ front-end/superfluous-weather-app/main.js
@@ -1,2 +1,24 @@
 import './style.css'
 
+// Select the button using its id
+let forecastButton = document.getElementById('forecast-button');
+
+// Add event listener to the button
+forecastButton.addEventListener('click', function() {
+  // URL for the GET request
+  let url = 'http://localhost:8080/weather-forecast?location=hartwell , GA';
+
+  // Fetch data from the URL
+  fetch(url)
+    .then(response => response.json()) // Transform the data into json
+    .then(data => {
+      // Do something with the data
+      console.log(data);
+    })
+    .catch(error => {
+      // Handle the error
+      console.error('Error:', error);
+    });
+});
+
+
